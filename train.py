9f11785a4d127bc9fa2ca64257c04208ef7fa97b
Zheng Wei <zhengw9@illinois.edu>
2024-05-04 09:58:59 -0500

    update notebook

--- train.py
+++ train.py
@@ -15,11 +15,11 @@ def print_model_parameters(model):
         if param.requires_grad:
             print(name, param.data)
 
-def train(net, train_loader, criterion, optimizer, num_epochs, report_interval = 5):
+def train(net, train_loader, criterion, optimizer, num_epochs, report_interval = 5, isDebug = True):
     net.train()
     running_loss = 0
     for epoch in range(num_epochs):
-        print("Epoch", epoch)
+        print("Epoch", epoch+1)
         # for i, data in enumerate(train_loader):
             # colorimages, _ = data            
 
@@ -45,19 +45,20 @@ def train(net, train_loader, criterion, optimizer, num_epochs, report_interval =
             l_channel = torch.tensor(np.expand_dims(gray_images, axis=-1))
 
 
-
-            # print("l_shape")
-            # print(l_channel.shape)
-            # print("res_shape")
-            # print(results.shape)
+            if isDebug:
+                print("l_shape")
+                print(l_channel.shape)
+                print("res_shape")
+                print(results.shape)
             reshaped_results = results.view(results.size(0), 256, 256, 2) * 255.0
 
             #lab_output = torch.from_numpy(np.concatenate((l_channel, reshaped_resultsd.detach().numpy()), axis=3))
             lab_output = torch.cat((l_channel, reshaped_results), dim = 3)
             
-            # print("lab_output shape:", lab_output.shape)
-            # print("color_images shape:", color_images.shape)
-            # print(criterion)
+            if isDebug:
+                print("lab_output shape:", lab_output.shape)
+                print("color_images shape:", color_images.shape)
+                print(criterion)
 
 
             loss = criterion(lab_output, color_images)
@@ -66,19 +67,20 @@ def train(net, train_loader, criterion, optimizer, num_epochs, report_interval =
 
             running_loss += loss.item()
             if i % report_interval + 1 == report_interval:
-                # print("chan_l")
-                # print(l_channel[0,:5,:5,0])
-                # print("chan_a")
-                # print(reshaped_results[0,:5,:5,0])
-                # print("chan_b")
-                # print(reshaped_results[0,:5,:5,1])
-                # print("cat_chan_l")
-                # print(lab_output[0,:5,:5,0])     
-                # print("cat_chan_a")
-                # print(lab_output[0,:5,:5,1])
-                # print("cat_chan_b")
-                # print(lab_output[0,:5,:5,2])
-                # print_model_parameters(net)
+                if isDebug:
+                    print("chan_l")
+                    print(l_channel[0,:5,:5,0])
+                    print("chan_a")
+                    print(reshaped_results[0,:5,:5,0])
+                    print("chan_b")
+                    print(reshaped_results[0,:5,:5,1])
+                    print("cat_chan_l")
+                    print(lab_output[0,:5,:5,0])     
+                    print("cat_chan_a")
+                    print(lab_output[0,:5,:5,1])
+                    print("cat_chan_b")
+                    print(lab_output[0,:5,:5,2])
+                    print_model_parameters(net)
                 print('avg loss here: ' + str(running_loss / report_interval))
                 running_loss = 0
 
